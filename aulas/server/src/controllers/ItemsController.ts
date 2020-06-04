ebfef0ea344d0b9421a6bc06839b1bfeeebb733f
lucasgcb <lucascoliado@hotmail.com>
2020-06-04 17:39:16 -0300

    Add Host variable

--- aulas/server/src/controllers/ItemsController.ts
+++ aulas/server/src/controllers/ItemsController.ts
@@ -1,6 +1,6 @@
 import { Request, Response, request } from 'express';
 import knex from '../database/connection';
-
+const HOST = "192.168.0.35" // Encontrar automaticamente no futuro
 class ItemsController {
     async index(request: Request, response: Response) {
         const items = await knex('items').select('*');
@@ -8,7 +8,7 @@ class ItemsController {
             return {
                 id: item.id,
                 title: item.title,
-                img_url: `http://localhost:3333/uploads/${item.image}`,
+                img_url: `http://${HOST}:3333/uploads/${item.image}`,
             };
         })
         return response.json(serializedItems);
